dff298421a9fad98e558d0cad5111f015f619a62
GnourtNaux <caoxuantruong1999@gmail.com>
2023-04-18 15:36:37 +0700

    april-11

--- js-practices/calculator/assets/js/index.js
+++ js-practices/calculator/assets/js/index.js
@@ -1,6 +1,9 @@
 // Console-button
 let inputResult = document.getElementsByClassName("input-result");
 let outputResult = document.getElementsByClassName("output-result");
+let historyFirst = document.getElementsByClassName("historyFirst");
+let historySecond = document.getElementsByClassName("historySecond");
+let historyThird = document.getElementsByClassName("historyThird");
 
 let inputValueFirst = "";
 let inputValueSecond = "";
@@ -17,13 +20,54 @@ const addNumber = (number) => {
   } else {
     inputValueFirst += number.toString();
     inputResult[0].value = inputValueFirst;
-    console.log("haha", preValue, inputValueFirst);
   }
 };
 
-// const handleClear = () => {
-
-// };
+const handleClear = () => {
+  if (isNaN(inputValueFirst)) {
+    if (Boolean(inputValueSecond)) {
+      handleClearArr = inputValueSecond.split("");
+      handleClearArr.pop();
+      inputValueSecond = handleClearArr.join("");
+      inputResult[0].value = preValue + ` ${operator} ` + inputValueSecond;
+      console.log(
+        "Khi có in2 : ope = ",
+        operator,
+        "; Preval = ",
+        preValue,
+        "; input1 =",
+        inputValueFirst,
+        "; input2 = ",
+        inputValueSecond
+      );
+    } else {
+      handleClearArr = inputResult[0].value.split("");
+      handleClearArr.pop();
+      handleClearArr.pop();
+      handleClearArr.pop();
+      operator = "";
+      inputResult[0].value = handleClearArr.join("");
+      preValue = inputResult[0].value;
+      inputValueFirst = preValue;
+      // console.log(
+      //   "Khi không có in2 mà chỉ có ope : ope = ",
+      //   operator,
+      //   "; Preval = ",
+      //   preValue,
+      //   "; input1 =",
+      //   inputValueFirst,
+      //   "; input2 = ",
+      //   inputValueSecond
+      // );
+    }
+  } else {
+    handleClearArr = inputResult[0].value.split("");
+    handleClearArr.pop();
+    inputResult[0].value = handleClearArr.join("");
+    preValue = inputResult[0].value;
+    inputValueFirst = preValue;
+  }
+};
 
 const handleChangeValue = (data) => {
   console.log("data", data);
@@ -58,7 +102,7 @@ const addOperator = (operatorValue) => {
 };
 
 const handleEqual = () => {
-  if (isNaN(inputValueSecond) & (inputValueSecond === "")) {
+  if (isNaN(inputValueFirst) & (inputValueSecond === "")) {
     outputResult[0].value = "Error";
   } else {
     switch (operator) {
@@ -77,6 +121,14 @@ const handleEqual = () => {
       default:
         outputResult[0].value = Number(inputResult[0].value);
         break;
+    }
+    if (Boolean(historyFirst[0].value)) {
+      historySecond[0].value = historyFirst[0].value;
+      historyFirst[0].value =
+        inputResult[0].value + " = " + outputResult[0].value.toString();
+    } else {
+      historyFirst[0].value =
+        inputResult[0].value + " = " + outputResult[0].value.toString();
     }
     operator = "";
     preValue = outputResult[0].value;
